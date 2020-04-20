dff2346a129cf5a6f43722d8dcca816d5d1267af
Chirag Jain <jainchg@gmail.com>
2020-04-20 19:23:08 +0530

    Add Symptom tracker in Contact Tracer

--- app/src/main/java/inspire2connect/inspire2connect/contactTracer/MainActivity.java
+++ app/src/main/java/inspire2connect/inspire2connect/contactTracer/MainActivity.java
@@ -37,6 +37,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import inspire2connect.inspire2connect.QuestionsActivity;
 import inspire2connect.inspire2connect.R;
 import inspire2connect.inspire2connect.symptomActivity;
 import inspire2connect.inspire2connect.utils.BaseActivity;
@@ -270,7 +271,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 showLoginActivity();
                 break;
             case R.id.symptom_tracker_box:
-                Intent i=new Intent(MainActivity.this, symptomActivity.class);
+                Intent i=new Intent(MainActivity.this, QuestionsActivity.class);
                 startActivity(i);
                 break;
             case R.id.share_box:
