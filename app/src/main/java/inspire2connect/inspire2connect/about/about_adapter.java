3092d4f5998ff7eaa3459089d25d962471f6891b
Chirag Jain <jainchg@gmail.com>
2020-04-22 15:05:52 +0530

    Correct about View

--- app/src/main/java/inspire2connect/inspire2connect/about/about_adapter.java
+++ app/src/main/java/inspire2connect/inspire2connect/about/about_adapter.java
@@ -4,6 +4,7 @@ package inspire2connect.inspire2connect.about;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +17,12 @@ import inspire2connect.inspire2connect.R;
 
 public class about_adapter extends BaseAdapter {
     private final Context mContext;
-    private final String[] names;
-    private final String[] sals;
+//    private final String[] names;
+    private final aboutElem[] names;
 
-    public about_adapter(Context mContext,String[] names,String[] sals) {
+    public about_adapter(Context mContext,aboutElem[] names) {
         this.mContext = mContext;
         this.names = names;
-        this.sals = sals;
     }
 
     @Override
@@ -44,8 +44,8 @@ public class about_adapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        final String name = names[position];
-        final String sal = sals[position];
+        final String name = names[position].name;
+        final String sal = names[position].tag;
 
         // 2
         if (convertView == null) {
@@ -54,12 +54,12 @@ public class about_adapter extends BaseAdapter {
         }
 
         // 3
-        final TextView nameTextView = (TextView)convertView.findViewById(R.id.textview_abt_name);
-        final TextView authorTextView = (TextView)convertView.findViewById(R.id.textview_author);
+        final TextView nameTextView = convertView.findViewById(R.id.textview_abt_name);
+        final TextView authorTextView = convertView.findViewById(R.id.textview_author);
 
         // 4
         nameTextView.setText(name);
-        authorTextView.setText(sal);
+        authorTextView.setText(sal.replace(", ", "\n"));
 
         return convertView;
     }
