86488300c085f6f5edfbd268194375e9121f4c9b
Chirag Jain <jainchg@gmail.com>
2020-05-07 20:59:17 +0530

    Start Satya Chat Activity

---
+++ app/src/main/java/inspire2connect/inspire2connect/satyaChat/ChatActivity.java
@@ -0,0 +1,16 @@
+package inspire2connect.inspire2connect.satyaChat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import inspire2connect.inspire2connect.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
