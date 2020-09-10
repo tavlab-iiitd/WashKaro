2262e22d52597962f219ac8664755d77b8ac890b
vihaanmisra <misravihaan27@gmail.com>
2020-09-10 16:58:01 +0530

    ScoreActivity UI Updation

--- app/src/main/java/inspire2connect/inspire2connect/quiz/scoreActivity.java
+++ app/src/main/java/inspire2connect/inspire2connect/quiz/scoreActivity.java
@@ -1,6 +1,8 @@
 package inspire2connect.inspire2connect.quiz;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,14 +12,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import inspire2connect.inspire2connect.R;
 import inspire2connect.inspire2connect.home.homeActivity;
+import inspire2connect.inspire2connect.utils.BaseActivity;
 
-public class scoreActivity extends AppCompatActivity {
+public class scoreActivity extends BaseActivity {
     private TextView score;
     private Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStatusBarGradiant(this);
         setContentView(R.layout.activity_score);
 
         score = findViewById(R.id.sa_score);
@@ -25,6 +29,8 @@ public class scoreActivity extends AppCompatActivity {
 
         String score_str = getIntent().getStringExtra("SCORE");
         score.setText(score_str);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable ( Color.TRANSPARENT));
 
 
         done.setOnClickListener(new View.OnClickListener() {
